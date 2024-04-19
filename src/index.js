0f3ade84f3e9852554bdcb8c8181a02b8fdbe9c6
soniikot <soniikot@gmail.com>
2024-04-19 18:53:00 +0900

    disable add new project button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import { createProject, addProject, newProjectBtn } from './navigation';
-import {createTask, addTask, createTaskBtn, displayTheTask} from './task';
+import { addTask, createTaskBtn} from './task';
 
 createTaskBtn.addEventListener ('click', addTask )
 
@@ -11,6 +11,18 @@ createTaskBtn.addEventListener ('click', addTask )
 
 
 window.addEventListener('load', createProject);
+
+const input = document.getElementById('newProjectTitle');
+newProjectBtn.disabled = true;
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (input.value === "") {
+        newProjectBtn.disabled = true; //button remains disabled
+    } else {
+        newProjectBtn.disabled = false; //button is enabled
+    }
+}
 newProjectBtn.addEventListener('click', addProject);
 
 
