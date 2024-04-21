7795c3bd177ac6c306b8ef856e5cd2f7ed5ec1f4
soniikot <soniikot@gmail.com>
2024-04-22 07:07:03 +0900

    fix add new project button

--- src/navigation.js
+++ src/navigation.js
@@ -1,29 +1,47 @@
 let projects=['house','work','health'];
-let menuContainer = document.createElement("div"); 
+
+const menuContainer = document.getElementById("projectArray")
 export function createProject(){
 
-
-
 for(let i=0; i<projects.length; i++){ 
-  let button = document.createElement("button"); 
+ const button = document.createElement("button");
+ button.classList.add('projectBtn'); 
   button.textContent = projects[i];
-  button.classList.add('menuContainer');
   menuContainer.appendChild(button); 
-}
+  removeProject(button);
 
-document.getElementById("projectArray").appendChild(menuContainer); 
+ 
+};
+
+
+
 
 }
 
 export function addProject(){
   projects.push(newProjectTitle.value);
-  menuContainer.innerHTML = '';
-createProject();
+  
+  const button = document.createElement("button");
+  button.classList.add('projectBtn'); 
+   button.textContent = newProjectTitle.value;
+   menuContainer.appendChild(button); 
+   removeProject(button)
+
  }
+
+function removeProject(button){
+ const removeButton = document.createElement("button");
+ menuContainer.appendChild(removeButton);
+removeButton.innerHTML='x'; 
+removeButton.addEventListener('click', () => {
+  menuContainer.removeChild(button);
+  menuContainer.removeChild(removeButton);
+
+
+});
+}
+
 
 
 export const newProjectBtn = document.getElementById('newProjectBtn');
-
-
-
 
